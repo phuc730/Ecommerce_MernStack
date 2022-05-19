2eb05c9a8888ff5a7bc5b73ab5ba828b826a3a7e
phuc730 <66136174+phuc730@users.noreply.github.com>
2022-05-20 02:23:58 +0700

    authenticate for dashboard

--- Admin/src/RequestMethod.js
+++ Admin/src/RequestMethod.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const isToken = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-
+const isUser = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 const BASE_URL = "http://localhost:5000/api"
-const TOKEN = isToken ? isToken.accessToken : ""
+const TOKEN = isUser ? isUser.accessToken : ""
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
--- Admin/src/index.js
+++ Admin/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading="null" persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
